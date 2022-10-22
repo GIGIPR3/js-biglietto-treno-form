5813c9dce4b81f041874d05f0ae16ce1c6ceae3b
GIGIPR3 <gigiprencipe47@gmail.com>
2022-10-22 14:47:13 +0200

    .

--- main.js
+++ main.js
@@ -1,23 +1,69 @@
 
-let inputNameElement = document.getElementById("nominativo");
-console.log("input " + inputNameElement);
+let inputNameElement = document.getElementById('nominativo');
+console.log(inputNameElement);
 
-let inputKM = document.getElementById("kmDaPercorrere").value;
-console.log({ut})
+let inputKMElement = document.getElementById('kmDaPercorrere');
+console.log(inputKMElement);
 
-let inputState = document.getElementById("inputState").value;
+let etaElement = document.getElementById('inputEta');
+console.log(etaElement);
 
-let btnGenera = document.getElementById("btnGenera").value;
+let btnGenera = document.getElementById('btnGenera').value;
+
+/* const valueName = inputNameElement.value;
+
+const valueKm = inputKMElement.value;
+
+const valueEta = etaElement.value;
+
+const costoKM = 0.21;
+
+console.log(valueName);
+    console.log(valueKm);
+    console.log(valueEta);
+    console.log('costo km:', costoKM );
+
+
+        let prezzoBiglietto = valueKm * costoKM;
+        
+        let prezzoMaggiorenni = prezzoBiglietto * 0.60;
+        
+        let prezzoMinorenni = prezzoBiglietto * 0.80; */
 
 
 function priceCalc(){
 
     const valueName = inputNameElement.value;
 
-    const valueKm = inputKM.value;
+    const valueKm = inputKMElement.value;
 
-    const valueEta = inputState.value;
+    const valueEta = etaElement.value;
+
+    const costoKM = 0.21; 
 
 
-    console.log(valueName, valueKm, valueEta);
-}
+  console.log(valueName);
+    console.log(valueKm);
+    console.log(valueEta);
+    console.log('costo km:', costoKM );
+
+
+        let prezzoBiglietto = valueKm * costoKM;
+        
+        let prezzoMaggiorenni = prezzoBiglietto * 0.80;
+        
+        let prezzoMinorenni = prezzoBiglietto * 0.60;
+
+    
+    if (valueEta >= '18'){
+        prezzoBiglietto = prezzoMaggiorenni.toFixed(2);
+    }
+    
+    else if (valueEta < '18'){
+        prezzoBiglietto = prezzoMinorenni.toFixed(2);
+    }
+
+    document.getElementById( 'costoTicket' ).innerHTML = prezzoBiglietto + "$";
+
+    document.getElementById('risultatoNome').innerHTML = valueName;
+} 
